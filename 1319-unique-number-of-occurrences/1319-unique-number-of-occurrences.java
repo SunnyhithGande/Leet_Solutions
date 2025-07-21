3314fa8fc426f4e5e2bd06e0e3566f8f4c4815ff
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2025-07-21 23:53:52 +0530

    Time: 2 ms (98.05%), Space: 42.1 MB (33.28%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -4,9 +4,16 @@ class Solution
     {
         int n = arr.length;
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num:arr)
+        for(int num : arr)
         {
-            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.containsKey(num))
+            {
+                map.put(num,map.get(num)+1);
+            }
+            else
+            {
+                map.put(num,1);
+            }
         }
         HashSet<Integer> hs = new HashSet<>();
         for(int freq : map.values())
