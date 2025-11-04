b3cf65f4c89f46ef25e70ae0b537187c5e1fa259
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2025-11-04 22:54:25 +0530

    Time: 1 ms (100%), Space: 43.3 MB (6.63%) - LeetHub

--- 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.java
+++ 2659-number-of-even-and-odd-bits/2659-number-of-even-and-odd-bits.java
@@ -5,10 +5,8 @@ class Solution
         int arr[] = new int[2];
         int evenCount = 0;
         int oddCount = 0;
-        
         String bin = Integer.toBinaryString(n);
-        int pos = 0; // bit position starting from rightmost bit
-        
+        int pos = 0; 
         for (int i = bin.length() - 1; i >= 0; i--) 
         {
             if (bin.charAt(i) == '1') 
@@ -18,9 +16,8 @@ class Solution
             }
             pos++;
         }
-        
-        arr[0] = evenCount;  // even-position bits count
-        arr[1] = oddCount;   // odd-position bits count
+        arr[0] = evenCount;
+        arr[1] = oddCount; 
         return arr;
     }
 }
