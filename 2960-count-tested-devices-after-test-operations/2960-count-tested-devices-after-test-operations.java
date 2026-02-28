cfa712709c6480ec9aa9429b12bc45ed2980eb5e
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2026-02-28 13:42:06 +0530

    Time: 1 ms (54.15%), Space: 44.8 MB (87.05%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int countTestedDevices(int[] batteryPercentages) 
+    {
+        int res = 0;
+        for(int i=0;i<batteryPercentages.length;i++)
+        {
+            if(batteryPercentages[i] > 0)
+            {
+                res++;
+                for(int j=i+1;j<batteryPercentages.length;j++)
+                {
+                    batteryPercentages[j] = batteryPercentages[j] - 1;
+                }
+            }
+        }
+        return res;
+    }
+}
