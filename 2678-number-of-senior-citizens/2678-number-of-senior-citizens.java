1c4b7b0aa659bee37fbbdb2b6808f9ed8d14217f
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2026-02-28 13:05:01 +0530

    Time: 0 ms (100%), Space: 44 MB (82.69%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -6,7 +6,9 @@ class Solution
         for(int i=0;i<details.length;i++)
         {
             String res = details[i];
-            if((res.charAt(11) - '0') >= 6 && (res.charAt(12) - '0') > 0) cnt++;
+            int tens = res.charAt(11) - '0';
+            int ones = res.charAt(12) - '0';
+            if(tens > 6 || (tens == 6 && ones > 0)) cnt++;
         }
         return cnt;
     }
