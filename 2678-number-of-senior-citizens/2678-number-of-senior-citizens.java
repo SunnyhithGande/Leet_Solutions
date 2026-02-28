84a21701ee04d9c9d0c4b4caba21c8c97d4b5710
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2026-02-28 13:01:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -5,14 +5,8 @@ class Solution
         int cnt = 0;
         for(int i=0;i<details.length;i++)
         {
-            String str = details[i];
-            String res = str.substring(11, str.length() - 2);
-            int num = 0;
-            for(int k=0;k<res.length();k++)
-            {
-                num = num*10 + (res.charAt(k) - '0');
-            }
-            if(num > 60) cnt++;
+            String res = details[i];
+            if((res.charAt(11) - '0') >= 6 && (res.charAt(12) - '0') > 0) cnt++;
         }
         return cnt;
     }
