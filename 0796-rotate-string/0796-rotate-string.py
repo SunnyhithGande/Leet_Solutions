403306f22de1c4093cd0847811b7e7923671f988
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2026-05-04 19:39:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        print(s[::-1])
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        
