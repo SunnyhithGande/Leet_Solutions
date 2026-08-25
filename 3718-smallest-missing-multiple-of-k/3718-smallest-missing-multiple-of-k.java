8ca14b024b4fe9ffbc301750ecd82fec8c5c5e44
Sunnyhith Gande <165890829+SunnyhithGande@users.noreply.github.com>
2026-08-25 09:50:47 +0530

    Time: 1 ms (82%), Space: 44.8 MB (83.31%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,5 +1,17 @@
-class Solution {
-    public int missingMultiple(int[] nums, int k) {
-        
+class Solution 
+{
+    public int missingMultiple(int[] nums, int k) 
+    {
+        Set<Integer> set = new HashSet<>();
+        for(int x : nums)
+        {
+            if(x % k == 0) set.add(x);
+        }
+        for(int i=1;i<1000;i++)
+        {
+            int num = k*i;
+            if(!set.contains(num)) return num;
+        }
+        return 0;
     }
 }
